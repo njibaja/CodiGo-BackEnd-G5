082d5e8cffeb04a20e1f6376ea89447ec2487a90
Nicolas Jibaja Ramos <njibaja@gmail.com>
2022-01-18 15:27:42 -0500

    chore: inicio flask

---
+++ Sesion01/inicios-flask.py
@@ -0,0 +1,7 @@
+from flask import Flask
+
+#Variables de uso propio de python
+#__main__ esta variable srive para indicar si estamos en el archivo princiapl del proyeto
+app = Flask(__name__)
+
+app.run()
